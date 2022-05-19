89f02c484f3d523d6f4f4fde7c102040470f2727
howsun <howsun7@gmail.com>
2022-05-18 22:21:10 -0700

    add educations app and Subject model

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "djmashup.users",
-    # Your stuff: custom apps go here
+    "educations",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
